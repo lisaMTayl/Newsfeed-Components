7f677c3726b70392a4a642dbeeab7bb568919458
Lisa <lmkirk@gmail.com>
2019-06-19 18:34:43 -0400

    changed function to arrow function--expand button working

--- Article/Article.js
+++ Article/Article.js
@@ -1,18 +1,16 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
+
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
-
+    // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector('.expandButton');
-
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton = document.addEventListener('click', this.expandArticle())
-
+    this.expandButton.addEventListener('click', () =>this.expandArticle())
   }
 
   expandArticle() {
@@ -32,5 +30,5 @@ let articles = document.querySelectorAll('.article');
 
 articles.forEach(function (article) {
   return new Article(article);
-
 });
+
