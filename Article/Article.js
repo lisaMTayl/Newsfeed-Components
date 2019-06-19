f52478a48198a3f0ad7b3ed28e39807b546757f0
Lisa <lmkirk@gmail.com>
2019-06-19 18:01:54 -0400

    added meta charset to index.html

--- Article/Article.js
+++ Article/Article.js
@@ -5,11 +5,14 @@ class Article {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
+
     this.expandButton = this.domElement.querySelector('.expandButton');
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton = document.addEventListener('click', this.expandArticle())
+
   }
 
   expandArticle() {
